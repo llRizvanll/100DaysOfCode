ad73d8301cc99ae56165ce2b256d6a9157f87851
Rizvan Hawaldar <torizvan@gmail.com>
2021-02-15 22:01:12 +0530

    Create BestTimeToBuyAndSell3.java

---
+++ BestTimeToBuyAndSell3.java
@@ -0,0 +1,17 @@
+// Suppose we only have 0 money at first
+// The max if we've just sold 2nd stock so far.
+// The max if we've just buy  2nd stock so far.
+// The max if we've just sold 1nd stock so far.
+// The max if we've just buy  1st stock so far. 
+///Since release1 is initiated as 0, so release2 will always higher than release1.
+public int maxProfit(int[] prices) {
+        int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
+        int release1 = 0, release2 = 0;
+        for(int i:prices){                              
+            release2 = Math.max(release2, hold2+i);     
+            hold2    = Math.max(hold2,    release1-i);  
+            release1 = Math.max(release1, hold1+i);     
+            hold1    = Math.max(hold1,    -i);          
+        }
+        return release2; 
+    }
