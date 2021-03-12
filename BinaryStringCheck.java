583a2f4b1f8fcb60567483345907714e55dfab77
Rizvan Hawaldar <torizvan@gmail.com>
2021-03-12 23:13:16 +0530

    Create BinaryStringCheck.java

---
+++ BinaryStringCheck.java
@@ -0,0 +1,11 @@
+public class BinaryStringCheck{
+  
+  public boolean hasAllCodes(String s, int k) {
+        Set<String> seen = new HashSet<>();
+        for (int i = k; i <= s.length() && seen.size() < 1 << k; ++i) {
+            seen.add(s.substring(i - k, i));
+        }
+        return seen.size() == 1 << k;
+    }
+
+}
